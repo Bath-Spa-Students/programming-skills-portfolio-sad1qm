bc3ce4e9cf6855e05672c21c7df36165d0b1afbc
sad1qm <144208877+sad1qm@users.noreply.github.com>
2023-10-10 10:01:48 +0400

    exercise1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice.py
@@ -0,0 +1,3 @@
+print ("Name" , "Abdullah" ,12345)
+print("Father's Name " , " Fathima " , 6789) 
+print ("University" , " Bath spa ")
---
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -0,0 +1,4 @@
+print(2+3)
+print(67-24)
+ans = ((2*3/8+(23+t5)))
+print (int (ans))
