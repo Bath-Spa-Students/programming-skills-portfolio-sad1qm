1d38cb673efcaca41e72136ba6bcb915b9a45545
sad1qm <144208877+sad1qm@users.noreply.github.com>
2023-10-11 10:06:27 +0400

    exercise5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise5.py
@@ -0,0 +1,5 @@
+# Write a Python program which accepts the radius of a circle from the user and compute the area.
+r= int(input("input the radius : "))
+b= 3.141592653589793
+area= b*r*r
+print ("area=", area)
