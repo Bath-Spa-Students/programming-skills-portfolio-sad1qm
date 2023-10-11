06da99d4d3189568009d83598f99aa34c194134c
sad1qm <144208877+sad1qm@users.noreply.github.com>
2023-10-11 10:06:12 +0400

    exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise4.py
@@ -0,0 +1,6 @@
+# Write three strings in different variables and print the output as one string.
+First_name= "MOHAMMED"
+LAST_name= "ABDUL NASER"
+Middle_name= "NAJWAN"
+Full_name= f"{First_name} {Middle_name} {LAST_name}"
+print (Full_name)
